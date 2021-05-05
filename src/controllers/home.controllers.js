2fd9eb8e65f17610515329a59263fdd4d858bcf0
Devchrisar <christopher.ariasar@amigo.edu.co>
2021-05-04 19:09:18 -0500

    <feat>[Firebase/logout]: added logout button and logout function from firebase

--- src/controllers/home.controllers.js
+++ src/controllers/home.controllers.js
@@ -250,7 +250,14 @@ if (!firebase.apps.length) {
           }
             })
     });
-
+    //*firebase logout
+    const logout = divElement.querySelector('#logout');
+    logout.addEventListener('click',(e) =>{
+    e.preventDefault();
+    auth.signOut().then(() => {
+      console.log('logout')
+    })
+  })
 
     //* codigo del sidebar*/
         const showMenu = (toggleId, navbarId, bodyId) => {
